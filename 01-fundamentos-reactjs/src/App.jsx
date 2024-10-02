52131ad1b1b7f00e993919d2423f830fc892af5f
Buschjulia <4543188224@estudante.sed.sc.gov.br>
2024-10-02 19:59:12 -0300

    Header

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import {Post} from "./Post"
+import {Header} from './components/Header'
+import './global.css';
+
+import sty
+
+export function App() {
+   return (
+    <div>
+      <Header/>
+
+      <div className="wrapper"></div>      
+    </div>
+  )
+}
